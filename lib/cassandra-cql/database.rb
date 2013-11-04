f22eb54618f4cb909e35a2ae1133f40d10cbb529
Kenneth Hoffmann <kenneth@backupify.com>
2013-11-04 15:37:55 -0500

    Provide level of 'ONE' for CQL3 consistency.

--- lib/cassandra-cql/database.rb
+++ lib/cassandra-cql/database.rb
@@ -113,7 +113,7 @@ module CassandraCQL
 
     def execute_cql_query(cql, compression=CassandraCQL::Thrift::Compression::NONE)
       if use_cql3?
-        @connection.execute_cql3_query(cql, compression)
+        @connection.execute_cql3_query(cql, compression, CassandraCQL::Thrift::ConsistencyLevel::ONE)
       else
         @connection.execute_cql_query(cql, compression)
       end
